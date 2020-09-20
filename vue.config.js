b5a39ae9858e164d3c1122fdea833e2b9fb4c5b1
SamuelVE <samuelvera98@gmail.com>
2020-09-19 21:47:43 -0300

    created a new project

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-gh-pages/" : "/"
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
+}
